19f4fe4dce91a2edb2810c75ddfa3c7b73f7cd54
Scott Straughan <scott.straughan@codeplay.com>
2024-07-11 12:58:18 +0100

    Moved legacy routes into its own file to reduce bloat.

---
+++ src/app/app.legacy-routes.ts
@@ -0,0 +1,54 @@
+/*---------------------------------------------------------------------------------------------
+ *
+ *  Copyright (C) Codeplay Software Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *--------------------------------------------------------------------------------------------*/
+
+import { Routes } from '@angular/router';
+
+export const appLegacyRoutes: Routes = [
+  {
+    path: 'learn',
+    redirectTo: 'getting-started'
+  },
+  {
+    path: 'releases',
+    redirectTo: 'ecosystem/implementations'
+  },
+  {
+    path: 'projects',
+    redirectTo: 'ecosystem/projects'
+  },
+  {
+    path: 'research',
+    redirectTo: 'ecosystem/research'
+  },
+  {
+    path: 'sessions',
+    redirectTo: 'calendar'
+  },
+  {
+    path: 'events',
+    redirectTo: 'calendar'
+  },
+  {
+    path: 'summer-sessions/2020/sessions',
+    redirectTo: 'calendar'
+  },
+  {
+    path: 'summer-sessions/2021/sessions',
+    redirectTo: 'calendar'
+  }
+];
